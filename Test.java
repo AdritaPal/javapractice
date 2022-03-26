d31c894c2bc178161b443395ac6092828230cec9
Adrita Pal <adrita.pal0801@gmail.com>
2022-03-26 12:29:51 +0530

    testing

---
+++ Test.java
@@ -0,0 +1,23 @@
+package com.ey.training.basics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		int a[] = new int[5];
+
+		List<String> strList = Arrays.asList("Java","python","c++");
+		
+		for(String x : strList) {
+			
+			System.out.println(x);
+		}
+
+
+	}
+
+}
